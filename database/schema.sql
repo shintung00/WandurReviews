1e13e83316f4e83dd00f3fdb1ec365b1b3812c9a
Wenhao Piao <wenhaopiao520@gmail.com>
2020-01-25 22:55:11 -0800

    dev-env set up

---
+++ database/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS reviews;
+
+CREATE DATABASE reviews;
+
+USE reviews;
+
+CREATE TABLE reviews (
+    id INT NOT NULL AUTO_INCREMENT,
+    property_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    comments VARCHAR(5000) NOT NULL,
+    user_photo VARCHAR(250) NOT NULL,
+    PRIMARY KEY(id)
+);
---
+++ dummyData.js
@@ -0,0 +1,11 @@
+const faker = require('faker');
+
+const data = [];
+
+for (let i = 0; i < 100; i++) {
+    for (let j = 1; j < 31; j++) {
+        data.push({property_id: i, name: faker.name.findName(), comments: faker.lorem.sentences(), user_photo: faker.image.avatar()});
+    }
+}
+
+module.exports = data;
